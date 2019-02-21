89ca01728ef5a76cf27942e6bb010e27f3994509
est31 <MTest31@outlook.com>
2019-02-21 10:47:03 +0100

    Add copyright header to samples.rs

--- src/samples.rs
+++ src/samples.rs
@@ -1,3 +1,11 @@
+// Vorbis decoder written in Rust
+//
+// Copyright (c) 2019 est31 <MTest31@outlook.com>
+// and contributors. All rights reserved.
+// Licensed under MIT license, or Apache 2 license,
+// at your option. Please see the LICENSE file
+// attached to this source distribution for details.
+
 pub trait Samples {
 	fn num_samples(&self) -> usize;
 	fn truncate(&mut self, limit :usize);
