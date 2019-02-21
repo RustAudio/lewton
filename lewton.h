32e3b6d3453a2458ac7aaf6e7668ff8f7609189d
est31 <MTest31@outlook.com>
2019-02-21 10:47:50 +0100

    Add lewton.h that will contain the public C API

---
+++ lewton.h
@@ -0,0 +1,14 @@
+// Vorbis decoder written in Rust
+//
+// Copyright (c) 2019 est31 <MTest31@outlook.com>
+// and contributors. All rights reserved.
+// Licensed under MIT license, or Apache 2 license,
+// at your option. Please see the LICENSE file
+// attached to this source distribution for details.
+
+#ifndef LEWTON_H
+#define LEWTON_H
+
+// Lewton C API here
+
+#endif
